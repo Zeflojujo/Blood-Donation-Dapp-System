3f1e686cd11ce5432f911467f23367e778ff7411
josephat <zeflojujo@gmail.com>
2024-03-19 14:34:43 +0300

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./+homedirectory/pages/Home";
 
